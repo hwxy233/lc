3676f9c6ace63683557c060fedfa065724713c74
hwxy <lijiacheng.ye@gmail.com>
2021-07-03 16:58:11 +0800

    Feature: 多重部分和问题。

---
+++ src/main/java/com/hwxy/OJ/base/dfs/partsum/PartSum.java
@@ -0,0 +1,63 @@
+package com.hwxy.OJ.base.dfs.partsum;
+
+/**
+ * 干嘛的
+ *
+ * @author hwxy
+ * @date 2021/03/20
+ **/
+public class PartSum {
+
+    public static void main(String[] args) {
+        PartSum partSum = new PartSum();
+        System.out.println(partSum.checkPartSum(4, new int[]{1, 2, 4, 7}, 13));
+    }
+
+    /**
+     * 这次的数组
+     */
+    private int[] array;
+    /**
+     * 数组的元素数量
+     */
+    private int n;
+    /**
+     * 目标的值
+     */
+    private int target;
+
+    /**
+     * 检查是否有部分和
+     *
+     * @param n
+     * @param array
+     * @param target
+     * @return
+     */
+    public boolean checkPartSum(int n, int[] array, int target) {
+        this.array = array;
+        this.n = n;
+        this.target = target;
+        return partSum(0, 0);
+    }
+
+    /**
+     * 这次要计算的数组的位置
+     *
+     * @param i
+     * @param sum
+     * @return
+     */
+    boolean partSum(int i, int sum) {
+        // 说明数组遍历完了,要检查目标的值
+        if (i == n) {
+            return sum == target;
+        }
+        // 如果不加当前的数组的位置
+        if (partSum(i + 1, sum)) {
+            return true;
+        }
+        // 如果加了
+        return partSum(i + 1, sum + array[i]);
+    }
+}
---
+++ src/main/java/com/hwxy/order/l100/lc94/Lc94.java
@@ -0,0 +1,39 @@
+package com.hwxy.order.l100.lc94;
+
+import com.hwxy.type.tree.TreeNode;
+
+import java.util.*;
+
+public class Lc94 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, new TreeNode(2, null, new TreeNode(3, null, null)), null);
+        Lc94 lc94 = new Lc94();
+        System.out.println(lc94.inorderTraversal(root));
+    }
+
+    /**
+     * 中序遍历
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (Objects.isNull(root)) {
+            return Collections.emptyList();
+        }
+        List<Integer> result = new LinkedList<>();
+        doInorderTraversal(root, result);
+        return result;
+    }
+
+    private void doInorderTraversal(TreeNode root, List<Integer> result) {
+        if (Objects.isNull(root)) {
+            return;
+        }
+        doInorderTraversal(root.left, result);
+        // 在中间进行add操作
+        result.add(root.val);
+        doInorderTraversal(root.right, result);
+    }
+}
