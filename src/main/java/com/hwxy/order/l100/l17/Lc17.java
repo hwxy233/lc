690429d298d5e88e3305fc680182f6514483d07a
hwxy <lijiacheng.ye@gmail.com>
2021-07-11 19:14:06 +0800

    Feature: 回溯的组合问题lc17。

---
+++ src/main/java/com/hwxy/order/l100/l17/Lc17.java
@@ -0,0 +1,77 @@
+package com.hwxy.order.l100.l17;
+
+import java.util.*;
+
+/**
+ * 干嘛的
+ *
+ * @author hwxy
+ * @date 2021/07/11
+ **/
+public class Lc17 {
+
+    public static void main(String[] args) {
+        Lc17 lc17 = new Lc17();
+        System.out.println(lc17.letterCombinations("23"));
+    }
+
+    /**
+     * 键盘的映射关系
+     * 为什么不都是3个？？？
+     */
+    private final Map<Character, List<String>> keyBord;
+
+    /**
+     * 数字的组合情况
+     */
+    private final LinkedList<String> digitsResult = new LinkedList<>();
+
+    /**
+     * 数字的组合path
+     */
+    private StringBuilder digitsPath = new StringBuilder();
+
+    {
+        keyBord = new HashMap<>();
+        keyBord.put('1', Collections.emptyList());
+        keyBord.put('2', Arrays.asList("a", "b", "c"));
+        keyBord.put('3', Arrays.asList("d", "e", "f"));
+        keyBord.put('4', Arrays.asList("g", "h", "i"));
+        keyBord.put('5', Arrays.asList("j", "k", "l"));
+        keyBord.put('6', Arrays.asList("m", "n", "o"));
+        keyBord.put('7', Arrays.asList("p", "q", "r", "s"));
+        keyBord.put('8', Arrays.asList("t", "u", "v"));
+        keyBord.put('9', Arrays.asList("w", "x", "y", "z"));
+    }
+
+
+    public List<String> letterCombinations(String digits) {
+        if (Objects.isNull(digits) || digits.length() > 4 || digits.length() <= 0) {
+            return Collections.emptyList();
+        }
+        // 数字的组合只有一种就是顺着的
+        backtrackingDigits(digits, digits.length(), 0);
+        return digitsResult;
+    }
+
+    private void backtrackingDigits(String digits, int length, int startIndex) {
+        if (digitsPath.length() == length) {
+            digitsResult.add(digitsPath.toString());
+            return;
+        }
+        // 横着对数字(数字对应的字母),这个for循环意义不大了因为startIndex永远是i+1
+        // for (int i = startIndex; i < length; i++) {
+        // 数字对应的字母
+        List<String> keys = keyBord.get(digits.charAt(startIndex));
+        // 其实横着的是对应的字母
+        for (String key : keys) {
+            // 先把自己放进去
+            digitsPath.append(key);
+            // 然后遍历下个数字
+            backtrackingDigits(digits, length, startIndex + 1);
+            // 回溯
+            digitsPath.deleteCharAt(digitsPath.length() - 1);
+        }
+        // }
+    }
+}
--- src/main/java/com/hwxy/type/reverse/readme.md
+++ src/main/java/com/hwxy/type/reverse/readme.md
@@ -1,13 +1,22 @@
 # 回溯相关的题目目录
 
 ## 77. 组合
+
 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。  
 [https://leetcode-cn.com/problems/combinations/][lc 77]
 
 [lc 77]: https://leetcode-cn.com/problems/combinations/
 
 ## 216. 组合总和 III
+
 找出所有相加之和为 n 的 k 个数的组合。组合中只允许含有 1 - 9 的正整数，并且每种组合中不存在重复的数字。
 [https://leetcode-cn.com/problems/combination-sum-iii/][LC 216]
 
 [LC 216]: https://leetcode-cn.com/problems/combination-sum-iii/
+
+## 17. 电话号码的字母组合
+
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+[https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/][lc 17]
+
+[lc 17]: https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
