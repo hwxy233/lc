445490e95ca0af9f4329e4bf30e8e9509972dad8
hwxy <lijiacheng.ye@gmail.com>
2022-01-30 16:52:32 +0800

    Feature: 数组相关。

---
+++ src/main/java/com/hwxy/order/l100/l27/Lc27.java
@@ -0,0 +1,67 @@
+package com.hwxy.order.l100.l27;
+
+import java.util.Arrays;
+
+/**
+ * 27. 移除元素
+ * https://leetcode-cn.com/problems/remove-element/
+ *
+ * @author hwxy
+ * @date 2022/01/30
+ **/
+public class Lc27 {
+    public static void main(String[] args) {
+        int[] a = new int[] {0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println(new Lc27().removeElement(a, 2));
+        System.out.println(Arrays.toString(a));
+    }
+
+    public int removeElement(int[] nums, int val) {
+        // return f1(nums, val);
+        return f2(nums, val);
+    }
+
+    /**
+     * 双for循环
+     * 
+     * @param nums
+     * @param val
+     * @return
+     */
+    private int f1(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int size = nums.length;
+        for (int i = 0; i < size; i++) {
+            if (nums[i] == val) {
+                for (int j = i + 1; j < size; j++) {
+                    nums[j - 1] = nums[j];
+                }
+                size--;
+                i--;
+            }
+        }
+        return size;
+    }
+
+    /**
+     * 快慢指针
+     * 
+     * @param nums
+     * @param val
+     * @return
+     */
+    private int f2(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+            if (nums[fastIndex] != val) {
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+}
---
+++ src/main/java/com/hwxy/type/array/readme.md
@@ -0,0 +1,21 @@
+# 数组的题目目录
+
+## 704. 二分查找
+
+左闭右闭,循环不变量
+[https://leetcode-cn.com/problems/binary-search/][lc 704]
+
+## 27. 移除元素
+
+快慢指针(指针)
+[https://leetcode-cn.com/problems/remove-element/][lc 27]
+
+## 209. 长度最小的子数组
+
+滑动窗口(双指针)
+[https://leetcode-cn.com/problems/minimum-size-subarray-sum/][lc 209]
+
+## 59. 螺旋矩阵 II
+
+上行,右列,下行,左列都是左闭右开;offset缩圈;中间的元素计数赋值
+[https://leetcode-cn.com/problems/spiral-matrix-ii/][lc 59]
