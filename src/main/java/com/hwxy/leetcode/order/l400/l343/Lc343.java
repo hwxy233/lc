b6cbf98e51f8261e558ebcdefcd6be0fff59a9b0
hwxy <lijiacheng.ye@gmail.com>
2022-04-28 23:13:22 +0800

    Feature: Lc343.

--- src/main/java/com/hwxy/leetcode/order/l400/l343/Lc343.java
+++ src/main/java/com/hwxy/leetcode/order/l400/l343/Lc343.java
@@ -21,6 +21,16 @@ public class Lc343 {
      * @return
      */
     public int integerBreak(int n) {
-
+        if (n < 2) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[(i - j)]));
+            }
+        }
+        return dp[n];
     }
 }
