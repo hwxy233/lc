1d183a183728cc08d404db2f7a07c54b498ef5e0
hwxy <lijiacheng.ye@gmail.com>
2022-04-28 22:51:50 +0800

    Feature: Lc343 题目.

---
+++ src/main/java/com/hwxy/leetcode/order/l400/l343/Lc343.java
@@ -0,0 +1,26 @@
+package com.hwxy.leetcode.order.l400.l343;
+
+/**
+ * 343. 整数拆分
+ * https://leetcode-cn.com/problems/integer-break/
+ * 
+ * @author hwxy
+ * @date 2022/04/28
+ **/
+public class Lc343 {
+    /**
+     * dp
+     * 1. dp[i]拆分i的乘积的最大值
+     * 2. dp[i] = max{dp[i], (i-j) * j, j * dp[i-j]}
+     * (i-j) * j意思是拆成2个数的乘积
+     * j * dp[i-j]为拆成多个数的乘积(因为dp[i-j]代表着i-j的这个数的乘积最大值了一定是拆成了至少2个数)
+     * 3. dp[0]和dp[1]没有意义, dp[2]=1, i应当从3开始
+     * 4. 顺序遍历
+     *
+     * @param n
+     * @return
+     */
+    public int integerBreak(int n) {
+
+    }
+}
--- src/main/java/com/hwxy/utils/JsonUtil.java
+++ src/main/java/com/hwxy/utils/JsonUtil.java
@@ -1,8 +1,32 @@
-package com.hwxy.utils;/**
-* 干嘛的
-* @author hwxy
-*
-* @date 2022/04/28
-**/
+package com.hwxy.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * 干嘛的
+ * 
+ * @author hwxy
+ *
+ * @date 2022/04/28
+ **/
 public class JsonUtil {
+    public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    public static <T> T convertToT(String json, Class<T> tClass) {
+        try {
+            return OBJECT_MAPPER.readValue(json, tClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T convertToT(String json, TypeReference<T> tTypeReference) {
+        try {
+            return OBJECT_MAPPER.readValue(json, tTypeReference);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
